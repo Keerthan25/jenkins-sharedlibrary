b155451af1e0f752ca69843c6055b29d492b5993
Keerthan25 <116776724+Keerthan25@users.noreply.github.com>
2023-02-21 13:27:51 +0530

    Update eksShellPlugin.groovy

--- vars/eksShellPlugin.groovy
+++ vars/eksShellPlugin.groovy
@@ -40,7 +40,7 @@ environment {
         stage('DEPLOY IMAGE') {
 		      steps {
 			      script {
-      withKubeConfig([credentialsId: 'kubernetesConfig', serverUrl: '']) {
+      withKubeConfig(credentialsId: 'kubeconfig', serverUrl: '')  {
                 sh 'kubectl set image deploy $deployment $containerName="$registry:$dockerTag" --record'
               }
             }
